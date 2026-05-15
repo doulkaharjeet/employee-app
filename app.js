106c3e6bd3ef7560d2c2ac065845fc079e14caa0
Harjeet Doulka <harjeetdoulka21@gmail.com>
2026-05-15 20:51:19 +0530

    comments for information

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var app = angular.module("employeeApp", ["ngRoute"]);
+//Main Module + Routing Engine
+
+var app = angular.module("employeeApp", ["ngRoute"]); 
+//Create my app called employeeApp and enable page routing.
 
 app.config(function($routeProvider) {
 
     $routeProvider
-
+//Load this file, use this controller
     .when("/", {
         templateUrl: "views/home.html",
         controller: "HomeController"
     })
-
+//Load this file, use this controller
     .when("/employees", {
         templateUrl: "views/employees.html",
         controller: "EmployeeController"
     })
-
+//If route not found → send user back home
     .otherwise({
         redirectTo: "/"
     });
